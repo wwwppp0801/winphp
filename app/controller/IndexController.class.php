a23857758679f0b5e949ada23fe74703154fef95
wang peng <wwwppp0801@qq.com>
2013-05-28 20:44:06 +0800

    apply

--- app/controller/IndexController.class.php
+++ app/controller/IndexController.class.php
@@ -29,23 +29,46 @@ class IndexController extends BaseController{
         return array('view'=>"index_apply.tpl",'model'=>array(
             'user'=>$_SESSION['user'],
             'msg'=>WinRequest::getFlash("msg"),
+            'phone'=>WinRequest::getParameter("phone"),
+            'email'=>WinRequest::getParameter("email"),
+            'awareness'=>WinRequest::getParameter("awareness"),
         ));
     }
     public function addApplyAction(){
-        DB::insert('insert into apply(openid,phone,email,awareness,question_url1,question_url2,question_url3,question_title1,question_title2,question_title3) values (?,?,?,?,?,?,?,?,?,?);',
-            $_SESSION['user']['openid'],
-            WinRequest::getParameter("phone"),
-            WinRequest::getParameter("email"),
-            WinRequest::getParameter("awareness"),
-            WinRequest::getParameter("question_url1"),
-            WinRequest::getParameter("question_url2"),
-            WinRequest::getParameter("question_url3"),
-            WinRequest::getParameter("question_title1"),
-            WinRequest::getParameter("question_title2"),
-            WinRequest::getParameter("question_title3")
+        if(strlen(WinRequest::getParameter("phone"))<5){
+            WinRequest::setFlash("msg","请填入正确的手机号");
+        }else if(strpos(WinRequest::getParameter("email"),"@")===false){
+            WinRequest::setFlash("msg","请填入正确的邮箱");
+        }elseif(strlen(trim(WinRequest::getParameter("awareness")))==0){
+            WinRequest::setFlash("msg","您对养乐多产品的是否了解?");
+        }elseif(
+            strlen(trim(WinRequest::getParameter("question_title1")))==0
+            && strlen(trim(WinRequest::getParameter("question_url1")))==0
+            && strlen(trim(WinRequest::getParameter("question_title2")))==0
+            && strlen(trim(WinRequest::getParameter("question_url2")))==0
+            && strlen(trim(WinRequest::getParameter("question_title3")))==0
+            && strlen(trim(WinRequest::getParameter("question_url3")))==0
+        ){
+            WinRequest::setFlash("msg",'请提供您亲自在搜搜问问下面的"肠道健康"标签里面回答过的关于肠道健康相关问题的标题和链接，最少1条');
+        }else{
+            DB::insert('insert into apply(openid,phone,email,awareness,question_url1,question_url2,question_url3,question_title1,question_title2,question_title3) values (?,?,?,?,?,?,?,?,?,?);',
+                $_SESSION['user']['openid'],
+                WinRequest::getParameter("phone"),
+                WinRequest::getParameter("email"),
+                WinRequest::getParameter("awareness"),
+                WinRequest::getParameter("question_url1"),
+                WinRequest::getParameter("question_url2"),
+                WinRequest::getParameter("question_url3"),
+                WinRequest::getParameter("question_title1"),
+                WinRequest::getParameter("question_title2"),
+                WinRequest::getParameter("question_title3")
+            );
+            WinRequest::setFlash("msg","申请成功！");
+        }
+        return array('redirect:/index/apply?phone='.urlencode(WinRequest::getParameter("phone"))
+            ."&email=".urlencode(WinRequest::getParameter("email"))
+            ."&awareness=".urlencode(WinRequest::getParameter("awareness"))
         );
-        WinRequest::setFlash("msg","申请成功！");
-        return array('redirect:/index/apply');
     }
     public function darenAction(){
         return array('view'=>"index_daren.tpl",'model'=>array(
--- template/index_apply.tpl
+++ template/index_apply.tpl
@@ -12,11 +12,11 @@
                 <span class="list">请完整填写以下必选信息，申请结果会在活动结束后直接公布在《达人榜》</span>
             	<div class="title"><span class="icon"></span>填写内容:</div>
                 <span class="list"><span class="num">1、</span>（必选）您的手机号码（工作人员将通过这种方式通知您获奖，手机号码不会对外公布，请放心填写。）:</span>
-                <span class="list"><input name="phone" type="text" class="input1" /></span>
+                <span class="list"><input value="{%$phone|escape%}" name="phone" type="text" class="input1" /></span>
                 <span class="list"><span class="num">2、</span>（必选）您的电子邮箱：</span>
-                <span class="list"><input name="email" type="text" class="input1" /></span>
+                <span class="list"><input value="{%$email|escape%}" name="email" type="text" class="input1" /></span>
                 <span class="list"><span class="num">3、</span>（必选）您对养乐多产品的是否了解（了解或者不了解）</span>
-                <span class="list"><input name="awareness" type="text" class="input1" /></span>
+                <span class="list"><input value="{%$awareness|escape%}" name="awareness" type="text" class="input1" /></span>
             </div>
 			<div class="right">
                 <span class="list"><span class="num">4、</span>（必选）请提供您亲自在搜搜问问下面的"肠道健康"标签里面回答过的关于肠道健康相关问题的标题和链接，最少1条，最多3条.</span>
