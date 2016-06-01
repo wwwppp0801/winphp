67c7fb253128580f9f027c7e58cecca7375d61c6
Wang Peng <wwwppp0801@gmail.com>
2016-06-01 17:50:13 +0000

    remove docker conf

--- clean.sh
+++
@@ -1,1 +0,0 @@
-sudo docker ps -a|awk 'NR>1{print $1}'|xargs sudo docker rm -f
--- run.sh
+++
@@ -1,2 +0,0 @@
-sudo docker run -d -v $(pwd):/root/winphp:rw --name winphp-php-fpm winphp-php-fpm /usr/sbin/php-fpm7.0 -F -y /root/winphp/php-fpm.conf
-sudo docker run -d -v $(pwd):/root/winphp:rw -v $(pwd)/nginxlog:/var/log/nginx --name winphp-nginx -p 8888:8888 --link winphp-php-fpm:php-fpm.server winphp-nginx /usr/sbin/nginx -c /root/winphp/nginx.conf -g "daemon off;" 
